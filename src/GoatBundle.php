f0189ac1ee0b637cc11459acf82fbc95926cb5bf
Pierre Rineau <pierre.rineau@makina-corpus.com>
2019-02-05 11:12:27 +0100

    changes journal services names and add a journal configuration pass

--- src/GoatBundle.php
+++ src/GoatBundle.php
@@ -8,6 +8,7 @@ use Goat\Bridge\Symfony\DependencyInjection\GoatExtension;
 use Goat\Bridge\Symfony\DependencyInjection\Compiler\HydratorPass;
 use Goat\Bridge\Symfony\DependencyInjection\Compiler\RegisterConverterPass;
 use Goat\Domain\DependencyInjection\Compiler\DispatcherConfigurationPass;
+use Goat\Domain\DependencyInjection\Compiler\JournalConfigurationPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -23,6 +24,9 @@ final class GoatBundle extends Bundle
     {
         $container->addCompilerPass(new HydratorPass());
         $container->addCompilerPass(new RegisterConverterPass());
+        if (\class_exists(JournalConfigurationPass::class)) {
+            $container->addCompilerPass(new JournalConfigurationPass());
+        }
         if (\class_exists(DispatcherConfigurationPass::class)) {
             $container->addCompilerPass(new DispatcherConfigurationPass());
         }
