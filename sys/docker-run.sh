b9b9311a17fb919910cb705a468727d4d3e282e0
Pierre Rineau <pierre.rineau@makina-corpus.com>
2022-02-07 13:23:52 +0100

    flattend and simplified unit testing docker configuration

--- sys/docker-run.sh
+++ sys/docker-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Running tests on PHP 7.4"
-APP_DIR="`dirname $PWD`" docker-compose -p goat_testing run php74 vendor/bin/phpunit "$@"
-
-echo "Running tests on PHP 8"
+echo "Running tests on PHP 8.0"
 APP_DIR="`dirname $PWD`" docker-compose -p goat_testing run php80 vendor/bin/phpunit "$@"
+
+echo "Running tests on PHP 8.1"
+APP_DIR="`dirname $PWD`" docker-compose -p goat_testing run php81 vendor/bin/phpunit "$@"
